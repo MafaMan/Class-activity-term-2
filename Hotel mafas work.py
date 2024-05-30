04ff85fd634bc396138140341305c72090099612
MafaMan <mafaneutron@gmail.com>
2024-05-30 10:08:10 +0200

    Add files via upload

---
+++ Hotel mafas work.py
@@ -0,0 +1,16 @@
+input("Name:")
+input("Surname:")
+input("Email:")
+
+print("Price list")
+price_list=["Doulbe=R600", "Triple=R750", "Quad=R1100", "Queen=R1500"]
+for item in price_list:
+    print(item)
+
+print("Type of Rooms")
+my_list=["Doulbe:", "Triple:", "Quad:", "Queen:"]
+for item in my_list:
+    input(item)
+
+
+
